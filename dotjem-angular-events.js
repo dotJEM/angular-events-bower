13c825bdd149b959f9e8eea48014a13862f20f7c
Jens Melgaard <admin@it-links.dk>
2016-06-08 13:50:59 +0200

    v0.0.2

--- dotjem-angular-events.js
+++ dotjem-angular-events.js
@@ -1,6 +1,137 @@
 /// <reference path="../typings/index.d.ts" />
 (function () {
     var module = angular.module('dotjem.angular.events', []);
+    /**
+     * @ngdoc constructor
+     * @name dxObservable
+     * @requires $rootScope
+     * @requires $q
+     *
+     * @description
+     *
+     * The constructor for an observable object.
+     *
+     * Instead of broadcasting events over the $rootScope for all to all, this provides a more
+     * direct pr. service approach.
+     *
+     * ```js
+     *
+     *   function myService(dxObservable) {
+     *
+     *      this.myEvent = new dxObservable();
+     *
+     *      this.doSomethingThatNotifies = function(){
+     *          // Do that something.
+     *          // ...
+     *
+     *          // Then notify...
+     *          this.myEvent.notify('some', 'stuff');
+     *      }
+     *   }
+     * ```
+     *
+     * @returns {dxObservable} An observable object.
+     */
+    module.service('dxObservable', ['$rootScope', '$q', function ($rootScope, $q) {
+            function Observable() {
+                var self = this;
+                var subscribers = [];
+                /**
+                 * @ngdoc method
+                 * @name dxObservable#waitForSingleCallback
+                 * @description
+                 *
+                 * Creates and returns a promise that will wait for the next time notify is called to be resolved.
+                 *
+                 * @returns a {Promise} that will get resolved next time notify is called.
+                 */
+                self.waitForSingleCallback = function () {
+                    var defer = $q.defer();
+                    var sub = self.subscribe(function () {
+                        defer.resolve();
+                        sub.dispose();
+                    });
+                    return defer.promise;
+                };
+                /**
+                 * @ngdoc method
+                 * @name dxObservable#subscribe
+                 * @description
+                 *
+                 * Subscribes for notifications from the observable object.
+                 *
+                 * @param {Function} callback to register in the subscriptions.
+                 *
+                 * @returns a {ISubscribtion} that can be used to unsubscribe from the observable object.
+                 */
+                self.subscribe = function (callback) {
+                    var handler = new Handler(callback);
+                    subscribers.push(handler);
+                    return {
+                        dispose: function () {
+                            var idx = subscribers.indexOf(handler);
+                            if (idx !== -1) {
+                                subscribers.splice(idx, 1);
+                            }
+                        }
+                    };
+                };
+                /**
+                 * @ngdoc method
+                 * @name dxObservable#unsubscribe
+                 * @description
+                 *
+                 * Removes a callback handler from the from the observable object.
+                 *
+                 * @param {Function} callback to unregister from the subscriptions..
+                 */
+                self.unsubscribe = function (callback) {
+                    //TODO: 'index of' then remove single.
+                    subscribers = subscribers.filter(function (handler) {
+                        return !handler.is(callback);
+                    });
+                };
+                /**
+                 * @ngdoc method
+                 * @name dxObservable#notify
+                 * @description
+                 *
+                 * Notifies all listeners.
+                 *
+                 * Handlers will receiver a {Event} object as the first argument.
+                 *
+                 * Any arguments passed to the notify function will be passed on to the any registered
+                 * handlers as the 1st to Nth+1 position.
+                 *
+                 * @param {...arguments} arguments for the handlers.
+                 */
+                self.notify = function () {
+                    var event = new Event();
+                    var args = [event].concat(arguments);
+                    return $rootScope.$apply(function () {
+                        return subscribers.every(function (handler) {
+                            handler.notify(args);
+                            return !event.$$defaultPrevented;
+                        });
+                    });
+                };
+                function Handler(callback) {
+                    this.notify = function (args) {
+                        callback.apply(callback, args);
+                    };
+                    this.is = function (other) {
+                        return callback === other;
+                    };
+                }
+                function Event() {
+                    this.$$defaultPrevented = false;
+                    this.preventDefault = function () {
+                        this.$$defaultPrevented = true;
+                    };
+                }
+            }
+            return Observable;
+        }]);
     module.directive('dxMouseWheel', ['$parse', function ($parse) {
             var events = 'mousewheel DOMMouseScroll MozMousePixelScroll';
             return {
